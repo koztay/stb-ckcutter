7ad0718511ad66cc1302cb78937f7bc26fe97a9b
koztay <koztay@me.com>
2017-07-07 17:28:31 +0300

    product detail template 'inde reformatting yapıldı, thumbnail_creator çok daha basitleştirild.

--- utils/thumbnail_creator.py
+++ utils/thumbnail_creator.py
@@ -5,75 +5,87 @@ from django.conf import settings
 from django.core.files import File
 import os
 import shutil
-from PIL import Image
+from PIL import Image, ImageOps
 import random
 
 
+def download_and_create_new_media_path_for_remote_images(media_path):
+    """
+    This function downloads images for given media path.
+    :param media_path: the path for the media which has to be downloaded
+    :return: download path for the image in order to create the thumbnail
+    """
+    print("I will download the file and use it as local")
+    temporary_download_location = settings.STATIC_ROOT + "/tmp"
+    filename = media_path.split("/")[-1]
+    temporary_download_path = temporary_download_location + filename
+    print(temporary_download_path)
+
+    # create tmp directory if not exists
+    if not os.path.exists(temporary_download_location):
+        os.makedirs(temporary_download_location)
+
+    if not os.path.exists(temporary_download_path):  # eğer file indirilmemişse indir.
+        # download the file
+        http = urllib3.PoolManager()
+        with http.request('GET', media_path, preload_content=False) as resp, open(temporary_download_path, 'wb') as out_file:
+            if resp.status is 200:
+                shutil.copyfileobj(resp, out_file)
+            else:
+                raise ValueError('A very specific bad thing happened. Response code was not 200')
+
+    return temporary_download_path
+
+
 def create_new_thumb(media_path, instance, owner_slug, max_width, max_height):
+    """
+    This function creates the thumbnails for image media_path passed from create_new_thumb() fucntion.
+    :param media_path: this path is the media path for the image. But I am not sure how this function does not work
+    if the download images is false in the task???
+    :param instance: this is the instance of the product which image has to be downloaded for.
+    :param owner_slug: this is the product slug which is used for creating media
+    :param max_width: max_width of the image, the image has been post processed for keeping the dimensions as this
+    :param max_height: max_height of the image, the image has been post processed for keeping the dimensions as this
+    :return: always True (why???) and creates the thumbnail images with its autoresize function.
+    """
     if media_path.startswith("http"):
-        print("I will download the file and use it as local")
-        temporary_download_location = settings.STATIC_ROOT + "/tmp"
-        filename = media_path.split("/")[-1]
-        temporary_download_path = temporary_download_location + filename
-        print(temporary_download_path)
+        media_path = download_and_create_new_media_path_for_remote_images(media_path)
 
-        # create tmp directory if not exists
-        if not os.path.exists(temporary_download_location):
-            os.makedirs(temporary_download_location)
-
-        if not os.path.exists(temporary_download_path):  # eğer file indirilmemişse indir.
-            # download the file
-            http = urllib3.PoolManager()
-            with http.request('GET', media_path, preload_content=False) as resp, open(temporary_download_path, 'wb') as out_file:
-                if resp.status is 200:
-                    shutil.copyfileobj(resp, out_file)
-                else:
-                    raise ValueError('A very specific bad thing happened. Response code was not 200')
-
-        # call the create new thumb func with new media path
-        create_new_thumb_local(temporary_download_path, instance, owner_slug, max_width, max_height)
-    else:
-        create_new_thumb_local(media_path, instance, owner_slug, max_width, max_height)
-
-
-def create_new_thumb_local(media_path, instance, owner_slug, max_width, max_height):
     filename = os.path.basename(media_path)
     filename = str(max_width) + "x" + str(max_height) + "-" + filename  # hep aynı isimde yazıyor yoksa
     print("filename : %s" % filename)
     thumb = Image.open(media_path)
-    width, height = thumb.size
-    # size = (max_height, max_width)
-    # aspect_ratio = width / height
+    # width, height = thumb.size mevcut size bilgisine gerek kalmadı...
 
-    # if width < max_width and height < max_height:  # her ikisi de kısa ise
-    #     # hangi tarafa doğru genişletmek gerek bul
-    #     if width / max_width < height / max_height:  # enden genişleyecek, boydan kırpılacak
-    #         # print("her ikisi de kısa, enden genişleyecek, boydan kırpılacak")
-    #         thumb = enden_genislet_boydan_kirp(width, height, max_width, max_height, thumb)
-    #     else:  # boydan genişleyecek, enden kırpılacak
-    #         # print("her ikisi de kısa, boydan genişleyecek, enden kırpılacak")
-    #         thumb = boydan_genislet_enden_kirp(width, height, max_width, max_height, thumb)
-    # elif width < max_width and height >= max_height:  # en küçük boy değil
-    #     # print("en kısa boy değil, enden genişleyecek, boydan kırpılacak")
-    #     thumb = enden_genislet_boydan_kirp(width, height, max_width, max_height, thumb)
-    # elif width >= max_width and height < max_height:  # boy küçük en değil
-    #     # print("boy kısa en değil, boydan genişleyecek, enden kırpılacak")
-    #     thumb = boydan_genislet_enden_kirp(width, height, max_width, max_height, thumb)
-    # elif width >= max_width and height >= max_height:  # her ikisi de büyük o yüzden resize yok.
-    #     # print("her ikisi de büyük o yüzden resize yok. Bu hatalı resize olacak yine")
-    #     if width / max_width > height / max_height:  # enden kırpılacak
-    #         # print("enden kırparak thumb yarat.")
-    #         thumb = enden_kirparak_thumb_yarat(width, height, max_width, max_height, thumb)
-    #     elif width / max_width < height / max_height:
-    #         # print("boydan kırparak thumb yarat.")
-    #         thumb = boydan_kirparak_thumb_yarat(width, height, max_width, max_height, thumb)
-    #     else:
-    #         new_size = (max_width, max_height)
-    #         thumb.thumbnail(new_size, Image.ANTIALIAS)
-    #         # print(thumb)
+    """
+    thumb = ImageOps.fit(image, size, Image.ANTIALIAS) bu yeni fonksiyonu deneiyoruz...
+    """
+    # method 1 (crops to fit):
+    # desired_size = (max_width, max_height)
+    # thumb = ImageOps.fit(thumb, desired_size, Image.ANTIALIAS)
 
-    # thumb = auto_resize_and_crop_image(width, height, max_width, max_height, thumb)
-    thumb = auto_resize_without_crop(width, height, max_width, max_height, thumb)
+    # method 2 (thumbnail methodu)
+    desired_size = max_width, max_height
+    thumb.thumbnail(desired_size, Image.ANTIALIAS)
+
+    def paste_to_background(image, size):
+        """
+        This function pastes the image into white background specified in the size parameter.
+        :param image: The image to be pasted into background
+        :param size: The background size
+        :return: the pasted image combined with the background image
+        """
+        background = Image.new('RGBA', size, (255, 255, 255, 0))
+        background.paste(
+            image, (int((size[0] - image.size[0]) / 2), int((size[1] - image.size[1]) / 2))
+        )
+        return background
+
+    bg_size = (max_width, max_height)
+
+    thumb = paste_to_background(thumb, bg_size)
+    # eski fonksiyon
+    # thumb = auto_resize_without_crop(width, height, max_width, max_height, thumb)
 
     temp_loc = "%s/%s/tmp" % (settings.MEDIA_ROOT + "/products", owner_slug)
     print("temp_loc : %s" % temp_loc)
@@ -94,74 +106,3 @@ def create_new_thumb_local(media_path, instance, owner_slug, max_width, max_heig
     instance.media.save(filename, thumb_file)
     shutil.rmtree(temp_loc, ignore_errors=True)
     return True
-
-
-#  Aşağıdaki algoritma en boy oranının çok orantısız olduğu durumlarda resmi kırpıyor ve tamamının
-#  gözükmesine olanak vermiyor.
-def auto_resize_and_crop_image(width, height, max_width, max_height, thumb):
-    width_ratio = max_width / width
-    height_ratio = max_height / height
-    resizing_factor = width_ratio / height_ratio
-    if __name__ == '__main__':
-        if resizing_factor > 1:  # boy eşit olana kadar resize et yani küçült yada büyüt yani heigth_ratio ile çarp
-            resize_ratio = width_ratio
-            new_width = int(width * resize_ratio)
-            new_heigth = int(height * resize_ratio)
-            new_size = (new_width, new_heigth)
-            thumb = thumb.resize(new_size, Image.ANTIALIAS)
-            crop_size = (new_heigth - max_height) / 2
-            left = 0
-            top = crop_size
-            width = left + max_width
-            height = top + max_height
-            box = (left, top, width, height)
-            thumb = thumb.crop(box)
-            return thumb
-
-        else:
-            resize_ratio = height_ratio  # eşitse ikisinden biri kadar büyütmek yeterli
-            new_width = int(width * resize_ratio)
-            new_heigth = int(height * resize_ratio)
-            new_size = (new_width, new_heigth)
-            thumb = thumb.resize(new_size, Image.ANTIALIAS)
-            crop_size = (new_width-max_width) / 2
-            left = crop_size
-            top = 0
-            width = left + max_width
-            height = top + max_height
-            box = (left, top, width, height)
-            thumb = thumb.crop(box)
-            return thumb
-
-
-# Bir de crop'suz algoritma yazabilirim, bu algoritma resmi her zaman kare olacak şekle resize eder.
-# Yani olması gereken aspecte resize etmek lazım. O zaman da arka planda beyaz bir resim açıp o resme
-# yapıştırmak lazım
-def auto_resize_without_crop(width, height, max_width, max_height, thumb):
-    max_size_aspect = max_width / max_height
-    thumb_aspect = width / height
-
-    if thumb_aspect > max_size_aspect:  # Resmi boydan uzatmak lazım
-        new_height = (width / max_size_aspect)
-        new_height = math.floor(new_height)
-        new_size = (width, new_height)
-        new_container_image = Image.new("RGB", new_size, "white")
-        left = 0
-        top = math.floor((new_height-height)/2)
-        box = (left, top)
-        new_container_image.paste(thumb, box)
-        thumb = new_container_image.resize((max_width, max_height), Image.ANTIALIAS)
-        return thumb
-    else:
-        new_width = math.floor(height * max_size_aspect)
-        new_size = (new_width, height)
-        new_container_image = Image.new("RGB", new_size, "white")
-        left = math.floor((new_width-width)/2)
-        top = 0
-        box = (left, top)
-        new_container_image.paste(thumb, box)
-        thumb = new_container_image.resize((max_width, max_height), Image.ANTIALIAS)
-        return thumb
-
-
-
